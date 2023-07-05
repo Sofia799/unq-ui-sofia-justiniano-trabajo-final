03c293cb42a4b11286dd5f3383e002a6256f4ce3
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-07-05 19:05:53 -0300

    agrego img logos en los botones

--- game-app/src/componentes/Juego.js
+++ game-app/src/componentes/Juego.js
@@ -1,14 +1,19 @@
 import React from "react";
-import "./Juego.css"
+import "./Juego.css";
+import papel from "../images/papel.png";
+import lagarto from "../images/lagarto.png";
+import piedra from "../images/piedra.png";
+import Spock from "../images/Spock.png";
+import tijeras from "../images/tijeras.png";
 
 const Juego = () => {
 
     const opciones = [
-        {id: 0, nombre: 'Piedra'},
-        {id: 1, nombre: 'Papel'},
-        {id: 2, nombre: 'Tijera'},
-        {id: 3, nombre: 'Lagarto'},
-        {id: 4, nombre: 'Spock'},
+        {id: 0, nombre: 'Piedra' , logo: piedra},
+        {id: 1, nombre: 'Papel' , logo: papel},
+        {id: 2, nombre: 'Tijera' , logo: tijeras},
+        {id: 3, nombre: 'Lagarto', logo: lagarto},
+        {id: 4, nombre: 'Spock', logo: Spock},
     ];
 
     return (
@@ -23,7 +28,7 @@ const Juego = () => {
                     {opciones.map((opcion) => {
                         return (
                             <button key={opcion.id} className="btn-opcion" > 
-                                {/* <img src="src/images/piedra.png"></img> */}
+                                <img className="img-opcion" src={opcion.logo} alt={opcion.nombre}></img>
                                 {opcion.nombre}
                             </button>
                         )
