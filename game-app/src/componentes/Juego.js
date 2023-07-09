31a7cf4cc2f611b53860df5b494b67f94282f3a5
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2023-07-09 18:04:06 -0300

    se agregan mas estilos, modificacion en las opciones y se muestra la opcion ganadora

--- game-app/src/componentes/Juego.js
+++ game-app/src/componentes/Juego.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Juego.css";
 import papel from "../images/papel.png";
 import lagarto from "../images/lagarto.png";
@@ -9,11 +9,46 @@ import tijeras from "../images/tijeras.png";
 const Juego = () => {
 
     const opciones = [
-        {id: 0, nombre: 'Piedra' , logo: piedra},
-        {id: 1, nombre: 'Papel' , logo: papel},
-        {id: 2, nombre: 'Tijera' , logo: tijeras},
-        {id: 3, nombre: 'Lagarto', logo: lagarto},
-        {id: 4, nombre: 'Spock', logo: Spock},
+        {   
+            id: 0, 
+            nombre: 'Piedra' , 
+            logo: piedra, 
+            jugarCon: ((rival) => {
+                return rival.nombre === 'Tijera' || rival.nombre === 'Lagarto' ? "Victoria" : "Derrota";
+            })
+        },
+        {
+            id: 1, 
+            nombre: 'Papel' , 
+            logo: papel,
+            jugarCon: ((rival) => {
+                return rival.nombre === 'Piedra' || rival.nombre === 'Spock' ? "Victoria" : "Derrota";
+            })
+        },
+        {
+            id: 2, 
+            nombre: 'Tijera' , 
+            logo: tijeras,
+            jugarCon: ((rival) => {
+                return rival.nombre === 'Papel' || rival.nombre === 'Lagarto' ? "Victoria" : "Derrota";
+            })
+        },
+        {
+            id: 3, 
+            nombre: 'Lagarto', 
+            logo: lagarto,
+            jugarCon: ((rival) => {
+                return rival.nombre === 'Spock' || rival.nombre === 'Papel' ? "Victoria" : "Derrota";
+            })
+        },
+        {
+            id: 4, 
+            nombre: 'Spock', 
+            logo: Spock,
+            jugarCon: ((rival) => {
+                return rival.nombre === 'Tijera' || rival.nombre === 'Piedra' ? "Victoria" : "Derrota";
+            })
+        },
     ];
 
     const [eleccionUser, setEleccionUser] = useState(null);
@@ -24,27 +59,42 @@ const Juego = () => {
     const [chosen, setChosen] = useState(false);
 
 
-    const handleEleccion = (e) => {
-        setEleccionUser(opciones ? opciones.find((opcion) => opcion.id === e) : <>No existe la opcion elegida</>);
+    const handleEleccion = (id) => {
+        let opcionElegidaUsuario = opciones ? opciones.find((opcion) => opcion.id === id) : <>No existe la opcion elegida</>;
+        setEleccionUser(opcionElegidaUsuario);
         setDisabled(true);
 
         setTimeout(() => {
             setChosen(true);
         }, 500)
-        
+      
         setDisabledOpcionesElegidas(false);
+        
+        const eleccionElegidaPc = eleccionRandom();
 
-        eleccionRandom();
+        setTimeout(() => {
+            resolverJugada(opcionElegidaUsuario, eleccionElegidaPc);
+        }, 5000)
     }
+
+    const resolverJugada = (opcionElegidaUsuario, eleccionElegidaPc) => {
+        if (opcionElegidaUsuario.nombre === eleccionElegidaPc.nombre) {
+            setResultado("Emplate");
+        } else {
+            setResultado(opcionElegidaUsuario.jugarCon(eleccionElegidaPc));
+        }
+    }
+
+    useEffect(() => {
+        console.log(resultado);
+
+    }, [resultado])
 
     const eleccionRandom = () => {
         const random = Math.floor(Math.random() * 5);
-
-        setTimeout(() => {
-            setEleccionPC(opciones ? opciones.find((opcion) => opcion.id === random) : <>No existe la opcion elegida</>)
-        }, 3000);
-
-        clearTimeout();
+        let opcionElegida = opciones ? opciones.find((opcion) => opcion.id === random) : <>No existe la opcion elegida</>;
+        setEleccionPC(opcionElegida);
+        return opcionElegida;
     }
 
     return (
@@ -55,11 +105,11 @@ const Juego = () => {
                             <h1 className="title-juego">¡Que comience el juego!</h1>
                         </div>
 
-                        <div  className="container-opciones">
+                        <div className="container-opciones">
                             <div className="box-btn-opciones">
                                 {opciones && opciones.map((opcion) => {
                                     return (
-                                        <button key={opcion.id} className={`btn-opcion ${disabled ? "disabled" : ""}`} onClick={() => handleEleccion(opcion.id)} disabled={disabled}> 
+                                        <button key={opcion.id} className={`btn-opcion btn-isHover ${disabled ? "disabled" : ""}`} onClick={() => handleEleccion(opcion.id)} disabled={disabled}> 
                                             <img className="img-opcion" src={opcion.logo} alt={opcion.nombre}></img>
                                             {opcion.nombre}
                                         </button>
@@ -69,8 +119,8 @@ const Juego = () => {
                         </div>
                 </div>)
             :
-                (<div className={`box-opciones-elegidas ${disabledOpcionesElegidas ? "disabled" : ""}`} disabled={disabledOpcionesElegidas}>
-                        <div className="box-opcion-elegida-user">
+                (<div className={`box-opciones-elegidas animated ${disabledOpcionesElegidas ? "disabled" : ""} ${resultado ? "fadeOutDown" : "" }`} disabled={disabledOpcionesElegidas}>
+                        <div className="bounceInLeft animated box-opcion-elegida-user">
                             {eleccionUser && (
                                 <button key={eleccionUser.id} className="btn-opcion"> 
                                     <img className="img-opcion" src={eleccionUser.logo} alt={eleccionUser.nombre}></img>
@@ -80,10 +130,10 @@ const Juego = () => {
                         </div>
 
                         <div className="box-vs">
-                            <p>VS</p>
+                            <h1 className="title-juego">VS</h1>
                         </div>
 
-                        <div className="box-opcion-elegida-pc">
+                        <div className="animated bounceInRight box-opcion-elegida-pc" disabled={true}>
                             {eleccionPC && (
                                 <button key={eleccionPC.id} className="btn-opcion"> 
                                     <img className="img-opcion" src={eleccionPC.logo} alt={eleccionPC.nombre}></img>
