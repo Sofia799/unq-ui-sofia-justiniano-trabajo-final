6f9fe7db938be3eb1697eecaaf83628cbe381dba
USERSAD\sojustin <sojustin@emeal.nttdata.com>
2023-07-11 21:21:39 -0300

    refactors de estilos

--- game-app/src/App.js
+++ game-app/src/App.js
@@ -1,16 +1,6 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Juego from './componentes/Juego';
 
-const App = () => {
-
-  return (
-    <Router>
-        <Routes>
-          <Route path="/" element={<Juego />} />
-        </Routes>
-    </Router>
-  )
-}
+const App = () => <Juego />
 
 export default App;
